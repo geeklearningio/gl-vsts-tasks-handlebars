a437a89c062206421e682f6dfd9451d955d44f61
Cyprien Autexier <cyprien.autexier@gmail.com>
2016-08-09 12:03:14 +0200

    temporarly add legacy postreleasetoslack task for reference

--- Tasks/ExecuteTemplates/executeTemplates.ts
+++ Tasks/ExecuteTemplates/executeTemplates.ts
@@ -1,3 +1,4 @@
 import path = require('path');
 import fs = require('fs-extra');
 import tl = require('vsts-task-lib/task');
+
--- Tasks/ExecuteTemplates/templatingEngine.ts
+++ Tasks/ExecuteTemplates/templatingEngine.ts
@@ -1,9 +1,20 @@
 import handlebars = require('handlebars');
+import path = require('path');
 
 export class TemplatingEngine {
 
-    constructor(templates: string[]) {
+    templates : {[key: string]: HandlebarsTemplateDelegate };   
 
+    constructor(templatesFiles: string[]) {
+        for (var index = 0; index < templatesFiles.length; index++) {
+            var templateFile = templatesFiles[index];
+            var templateFileName = path.basename(templateFile);
+            if (templateFileName[0] == "_"){
+                //andlebars.registerPartial()
+            } else {
+
+            }
+        }
     }
 
     apply(context: any): string {
